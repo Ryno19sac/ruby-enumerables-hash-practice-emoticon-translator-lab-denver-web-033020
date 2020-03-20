4a322a1e3177d139ba773c4709d1e55ed17c28db
Ryan Anderson <ryan.t.anderson19@gmail.com>
2020-03-20 20:30:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -65,5 +65,7 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
 end
 
 def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
+  
+  library = (load_library)
   # code goes here
 end
